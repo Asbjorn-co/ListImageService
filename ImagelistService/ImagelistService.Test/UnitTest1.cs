d1733aad38bdb616adf2e261b72a1541bed809f7
DenGuleDansker <chilinhm@gmail.com>
2024-03-04 13:29:38 +0100

    ændring

---
+++ ImagelistService/ImagelistService.Test/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace ImagelistService.Test;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
